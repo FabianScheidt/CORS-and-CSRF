932562cea0d386784fd3ca429ade860e8809fd25
Scheidt, Fabian <fabian.scheidt@umlaut.com>
2020-04-11 14:35:53 +0200

    Add landing page for different demos. Serve the same app on two ports.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { EchoController } from './echo/echo.controller';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, 'static'),
+      serveRoot: '/'
+    })
+  ],
   controllers: [
     EchoController
   ],
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function bootstrap(port) {
   const app = await NestFactory.create(AppModule);
   (app as any).disable('x-powered-by');
-  await app.listen(8080);
+  await app.listen(port);
 }
-bootstrap();
+bootstrap(4200);
+bootstrap(8080);
